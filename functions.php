94ae604584e1159c0844fd722f1cb5fb5ccc5c7e
romain Pieront <romain.p59680@hotmail.com>
2023-10-09 16:50:49 +0200

    Mise en place du Hook-menu-admin

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles()
 {
-    // Chargement du style.css du thème parent Twenty Twenty
+    // Chargement du style.css du thème parent BlankSlate
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     // Chargement du css/theme.css pour nos personnalisations
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/style.css', array(), filemtime(get_stylesheet_directory() . '/style.css'));
@@ -15,11 +15,15 @@ register_nav_menu('footer', 'Pied de page');
 
 /****FONCTION ADMIN ********/
 
-/****apply filters_add filters */ {
+function my_wp_nav_menu_args($args = '')
+{
     if (is_user_logged_in()) {
-        //code
+        // Logged in menu to display
+        $args['menu'] = 8;
     } else {
-        //code
+        // Non-logged-in menu to display
+        $args['menu'] = 25;
     }
+    return $args;
 }
-/**add_filter() / apply_filter()*****/
+add_filter('wp_nav_menu_args', 'my_wp_nav_menu_args');
